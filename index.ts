2af113fcd102d88f3541d15232d2a3de2b025e88
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-22 16:56:13 +0900

    section2-16

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
-let hasValue: boolean = true; //hasValueはtrueかfalseのどちらかの値を持つ
+let hasValue = true; //hasValueはtrueかfalseのどちらかの値を持つ
 let count: number = 10;
 let single: string = 'hello'
+
+let hello: string;
+hello = 2;
+hello = 'hello'
+hello = true
