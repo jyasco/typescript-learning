bab4edc7a383ce66d53898e39f72bdb39695feda
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-22 17:25:53 +0900

    section1-17

--- index.ts
+++ index.ts
@@ -3,6 +3,14 @@ let count: number = 10;
 let single: string = 'hello'
 
 let hello: string;
-hello = 2;
 hello = 'hello'
-hello = true
+
+const person = {
+  name: {
+    first: 'Jack',
+    last: 'Smith'
+  },
+  age: 21
+}
+
+console.log(person.name);
