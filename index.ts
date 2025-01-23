f41b8d71c23d44be6931241ca866184e1ba54c7f
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-01-23 15:47:37 +0900

    section2-29

--- index.ts
+++ index.ts
@@ -70,3 +70,12 @@ const anotherAdd: (n1: number, n2: number) => number = function(num1, num2){
 };
 
 const doubleNumber: (num: number) => number = num => num * 2;
+
+function doubleAndHandle(num: number, cd: (num: number) => number): void{
+  const doubleNum = cd(num * 2)
+  console.log(doubleNum);
+}
+
+doubleAndHandle(21, doubleNum => {
+  return doubleNum
+});
