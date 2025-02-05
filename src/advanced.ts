13a4e922dfe2b6a60efa5ecdba9d56c0dcf0361e
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-02-05 18:48:30 +0900

    section6-76

---
+++ src/advanced.ts
@@ -0,0 +1,21 @@
+interface Engineer {
+  name: string;
+  role: string;
+}
+
+interface Blogger {
+  name: string;
+  follower: number;
+}
+//type EnginnerBlogger = Engineer & Blogger;
+interface EnginnerBlogger extends Engineer, Blogger { }
+
+const quill: EnginnerBlogger = {
+  name: 'Quill',
+  role: 'front-end',
+  follower: 1000
+}
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber
