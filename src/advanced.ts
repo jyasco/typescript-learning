919f1dde55d7afe4867b19d80dc9b9334a80b9a5
鈴木茉莉花 <suzukimarika@suzukimarikanonotobukkukonpyuta.local>
2025-02-05 19:46:16 +0900

    section6-77

--- src/advanced.ts
+++ src/advanced.ts
@@ -19,3 +19,44 @@ const quill: EnginnerBlogger = {
 type NumberBoolean = number | boolean;
 type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber
+
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase();
+  }
+  return '';
+}
+
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker){
+  console.log(nomadWorker.name);
+  if ('role' in nomadWorker){
+    console.log(nomadWorker.role);
+  }
+  if ('followe' in nomadWorker) {
+    console.log(nomadWorker.followe);
+  }
+}
+
+class Dog {
+  speak() {
+    console.log('bow-wow');
+  }
+}
+
+class Bird {
+  speak() {
+    console.log('tweet-tweet');
+  }
+  fly() {
+    console.log('flutter');
+  }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet){
+  pet.speak
+  if (pet instanceof Bird){
+    pet.fly();
+  }
+}
