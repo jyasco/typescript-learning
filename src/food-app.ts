5967660c39a122feea8911282540c1d9e6b91d8d
jyasco <szkmrk715@outlook.jp>
2025-02-27 01:35:14 +0900

    section9-130

--- src/food-app.ts
+++ src/food-app.ts
@@ -3,6 +3,9 @@ class Score {
     const foods = new Foods();
     return foods.activeElementsScore.reduce((total, score) => total + score, 0)
   }
+  render(){
+    document.querySelector('.score__number')!.textContent = String(this.totalScore);
+  }
  }
 
 class Food { 
@@ -11,6 +14,8 @@ class Food {
   }
   clickEventHandler(){
     this.element.classList.toggle('food--active')
+    const score = new Score();
+    score.render();
   }
 }
 class Foods {
